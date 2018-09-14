72c645152442ca0e0962871fcb51683d72434db9
Roberto Alicata <roberto.alicata@gmail.com>
2018-09-14 11:01:25 +0200

    Add Photo Type. Generate Photo.id with uuid/v4

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const { ApolloServer } = require('apollo-server');
+const uuidv4 = require('uuid/v4');
 
 const typeDefs = `
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
+
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
   }
 
   type Mutation {
-    postPhoto(name: String! description: String): Boolean!
+    postPhoto(name: String! description: String): Photo!
   }
 `;
 
@@ -14,13 +23,18 @@ var photos = [];
 
 const resolvers = {
   Query: {
-    totalPhotos: () => photos.length
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos
   },
   Mutation: {
     postPhoto(parent, args) {
-      photos.push(args);
-      return true;
+      var newPhoto = { id: uuidv4(), ...args };
+      photos.push(newPhoto);
+      return newPhoto;
     }
+  },
+  Photo: {
+    url: parent => `http://localhost:4000/img/${parent.id}.jpg`
   }
 };
 
