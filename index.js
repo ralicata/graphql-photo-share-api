5f78823455e831c01db48539d393beca52634c34
Roberto Alicata <roberto.alicata@gmail.com>
2018-09-14 12:44:07 +0200

    Add taggedUsers and inPhotos

--- index.js
+++ index.js
@@ -10,12 +10,22 @@ const typeDefs = `
     GRAPHIC
   }
 
+  type User {
+    githubLogin: ID!
+    name: String!
+    avatar: String
+    postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
+  }
+
   type Photo {
     id: ID!
     url: String!
     name: String!
     description: String
     category: PhotoCategory!
+    postedBy: User!
+    taggedUsers: [User!]!
   }
 
   input PostPhotoInput {
@@ -34,7 +44,41 @@ const typeDefs = `
   }
 `;
 
-var photos = [];
+var users = [
+  { githubLogin: 'mHattrup', name: 'Mike Hattrup' },
+  { githubLogin: 'gPlake', name: 'Glen Plake' },
+  { githubLogin: 'sSchmidt', name: 'Scot Schmidt' }
+];
+
+var photos = [
+  {
+    id: '1',
+    name: 'Dropping the Heart Chute',
+    description: 'The heart chute is one of my favorite chutes',
+    category: 'ACTION',
+    githubUser: 'gPlake'
+  },
+  {
+    id: '2',
+    name: 'Enjoying the sunshine',
+    category: 'SELFIE',
+    githubUser: 'sSchmidt'
+  },
+  {
+    id: '3',
+    name: 'Gunbarrel 25',
+    description: '25 laps on gunbarrel today',
+    category: 'LANDSCAPE',
+    githubUser: 'sSchmidt'
+  }
+];
+
+var tags = [
+  { photoID: '1', userID: 'gPlake' },
+  { photoID: '2', userID: 'sSchmidt' },
+  { photoID: '2', userID: 'mHattrup' },
+  { photoID: '2', userID: 'gPlake' }
+];
 
 const resolvers = {
   Query: {
@@ -49,7 +93,31 @@ const resolvers = {
     }
   },
   Photo: {
-    url: parent => `http://localhost:4000/img/${parent.id}.jpg`
+    url: parent => `http://localhost:4000/img/${parent.id}.jpg`,
+    postedBy: parent => {
+      return users.find(u => u.githubLogin === parent.githubUser);
+    },
+    taggedUsers: parent =>
+      tags
+        // Returns an array of tags that only contains the current photo
+        .filter(tag => tag.photoID === parent.id)
+        // Converts the array of tags into an array of userIDs
+        .map(tag => tag.userID)
+        // Converts array of userIDs into an array of User objects
+        .map(userID => users.find(u => u.githubLogin === userID))
+  },
+  User: {
+    postedPhotos: parent => {
+      return photos.filter(p => p.githubUser === parent.githubLogin);
+    },
+    inPhotos: parent =>
+      tags
+        // Returns an array of tags that only contains the current user
+        .filter(tag => tag.userID === parent.id)
+        // Converts the array of tags into an array of photoIDs
+        .map(tag => tag.photoID)
+        // Converts array of photoIDs into an array of Photo objects
+        .map(photoID => photo.find(p => p.id === photoID))
   }
 };
 
